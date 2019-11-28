d4f5190f6a5081fe2973e7fa4e2dadcd01f30555
kbz2001 <kilezisme@gmail.com>
2019-11-27 22:00:25 -0600

    Updated + Finished setup.

--- src/main/java/me/kbz/nostalgia/Core.java
+++ src/main/java/me/kbz/nostalgia/Core.java
@@ -1,17 +1,16 @@
 package me.kbz.nostalgia;
 
+import net.minecraftforge.client.ClientCommandHandler;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.EventBus;
 
-@Mod(modid = Core.MODID, version = Core.VERSION)
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, acceptedMinecraftVersions = Reference.ACCEPTED_VERSIONS)
 public class Core {
-
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
-
     @EventHandler
     public void preinit(FMLPreInitializationEvent e) {
 
@@ -19,6 +18,12 @@ public class Core {
 
     @EventHandler
     public void init(FMLInitializationEvent e) {
+        EventBus eb = MinecraftForge.EVENT_BUS;
+
+
+
+        ClientCommandHandler cch = ClientCommandHandler.instance;
+
 
     }
 
